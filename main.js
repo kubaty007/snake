8fe924499f0ee26590bf6751af5af6cf1ffcc2d1
kubaty007 <jakub.mendel2002@gmail.com>
2022-01-19 15:03:31 +0100

    change direction, border colide, food

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const ctx = gameField.getContext("2d");
 
 let canvasBorder = 'green';
 let canvasBackground = 'lightgreen';
-let snakeColor = 'pink';
-let snakeBorder = 'red';
+let snakeColor = 'lightblue';
+let snakeBorder = 'blue';
+let appleColor = 'pink';
+let appleBorder = 'red';
 
-let snake = [
+const snake = [
     {x: 250, y: 250},
     {x: 240, y: 250},
     {x: 230, y: 250}
@@ -14,6 +16,13 @@ let snake = [
 
 let horizontalSpeed = 10;
 let verticalSpeed = 0;
+
+let str;
+
+let tiksSinceLastFood = 0;
+const betweenFoodPeroid = 50;
+
+const apples = [];
 
 function resetGameField(){
     ctx.fillStyle = canvasBackground;
@@ -41,12 +50,129 @@ function moveSnake(){
     snake.pop();
 }
 
+function changeDirection(direction){
+    if(direction === 'left'){
+        if(horizontalSpeed != 10){ //can't turn 180 degrees
+            horizontalSpeed = -10;
+            verticalSpeed = 0;
+        }
+    }
+    else if(direction === 'up'){
+        if(verticalSpeed != 10){
+            horizontalSpeed = 0;
+            verticalSpeed = -10;
+        }
+    }
+    else if(direction === 'right'){
+        if(horizontalSpeed != -10){
+            horizontalSpeed = 10;
+            verticalSpeed = 0;
+        }
+    }
+    else if(direction === 'down'){
+        if(verticalSpeed != -10){
+            horizontalSpeed = 0;
+            verticalSpeed = 10;
+        }
+    }
+}
+
+function isGameOver(){
+    if(snake[0].x < 0 || snake[0].x >= 500 || snake[0].y < 0 || snake[0].y >= 500){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function numberBetween(min, max){
+    return Math.floor(Math.random() * (max-min+1) + min);
+}
+
+function isFieldOccupied(x, y){
+    for(const snakeField of snake){
+        if((snakeField.x && snakeField.y) == (x, y)){
+            return true;
+        }
+    }
+    for(const appleField of apples){
+        if((appleField.x && appleField.y) == (x, y)){
+            return true;
+        }
+    }
+}
+
+function addFood(){
+    if(tiksSinceLastFood % betweenFoodPeroid === 0){
+        let newApple = {x: numberBetween(1, 50)*10, y: numberBetween(1, 50)*10};
+        if(!isFieldOccupied(newApple.x, newApple.y)){
+            apples.push(newApple);
+            console.log("dodano nowe jabłko: " + newApple.x + ", " + newApple.y);
+        }
+        else{
+            console.log('POLE ZAJETE', newApple.x, newApple.y);
+        }
+        
+    }
+    tiksSinceLastFood += 1;
+}
+
+function drawFoodPiece(apple){
+    ctx.fillStyle = appleColor;
+    ctx.strokeStyle = appleBorder;
+
+    ctx.fillRect(apple.x, apple.y, 10, 10);
+    ctx.strokeRect(apple.x, apple.y, 10, 10);
+}
+
+function drawFood(){
+    apples.forEach(drawFoodPiece);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.onkeydown = function key(e){
+    switch (e.keyCode){
+        case 37:
+            str = 'left';
+            break;
+        case 38:
+            str = 'up';
+            break;
+        case 39:
+            str = 'right';
+            break;
+        case 40:
+            str = 'down';
+            break;
+    }
+    changeDirection(str);
+};
+
 function main(){
     setTimeout(function onTick() {
         resetGameField();
         moveSnake()
         drawSnake();
-        main();
+        addFood();
+        drawFood();
+        if(!isGameOver()){
+            main();
+        }
+        else{
+            console.log('game over!');
+        }
     }, 100)
 }
 
