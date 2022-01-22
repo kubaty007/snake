f3d85de3ae60a7087bbef3d3a253c5cf0db9afa6
kubaty007 <jakub.mendel2002@gmail.com>
2022-01-22 15:05:01 +0100

    color changing, points

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const gameField = document.getElementById("gamefield");
 const ctx = gameField.getContext("2d");
 
+
+
 let canvasBorder = 'green';
 let canvasBackground = 'lightgreen';
 let snakeColor = 'lightblue';
@@ -8,7 +10,7 @@ let snakeBorder = 'blue';
 let appleColor = 'pink';
 let appleBorder = 'red';
 
-const snake = [
+let snake = [
     {x: 250, y: 250},
     {x: 240, y: 250},
     {x: 230, y: 250}
@@ -22,7 +24,10 @@ let str;
 let tiksSinceLastFood = 0;
 const betweenFoodPeroid = 50;
 
-const apples = [];
+let apples = [];
+
+let points;
+let record = 0;
 
 function resetGameField(){
     ctx.fillStyle = canvasBackground;
@@ -154,15 +159,68 @@ function isFoodBeingEaten(){
             //console.log('usuwanie: ' + apples[i].x + ' ' + apples[i].y);
             apples.splice(i, 1);
             increaseSizeOfSnake();
+            increaseScore();
         }
     }
 }
 
+function shadeColor(color, percent) {
 
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
 
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
 
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
 
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
 
+    return "#"+RR+GG+BB;
+}
+
+function changeColors(){
+    
+    canvasBackground = document.getElementById('canvasColor').value;
+    snakeColor = document.getElementById('snakeColor').value;
+    appleColor = document.getElementById('fruitColor').value;
+    
+    canvasBorder = shadeColor(canvasBackground, -40);
+    snakeBorder = shadeColor(snakeColor, -40);
+    appleBorder = shadeColor(appleColor, -40);
+}
+
+function restartGame(){
+
+    snake = [
+        {x: 250, y: 250},
+        {x: 240, y: 250},
+        {x: 230, y: 250}
+    ]
+    
+    apples = []
+
+    horizontalSpeed = 10;
+    verticalSpeed = 0;
+
+    main();
+}
+
+function increaseScore(){
+    points = snake.length - 3;
+    document.getElementById('aktualnePkt').innerHTML = points;
+
+    if(points > record){
+        record = points;
+        document.getElementById('maxPkt').innerHTML = record;
+    }
+}
 
 
 
